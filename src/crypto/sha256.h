fa8fe5cba6cac4adc13c815993aebaaafc194ddc
Joe Studio <dev@ntoken.org>
2018-03-05 10:53:50 +0800

    update algos

--- src/crypto/sha256.h
+++ src/crypto/sha256.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_CRYPTO_SHA256_H
-#define BITCOIN_CRYPTO_SHA256_H
+#ifndef CREDITS_CRYPTO_SHA256_H
+#define CREDITS_CRYPTO_SHA256_H
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -25,4 +25,4 @@ public:
     CSHA256& Reset();
 };
 
-#endif // BITCOIN_CRYPTO_SHA256_H
+#endif // CREDITS_CRYPTO_SHA256_H
